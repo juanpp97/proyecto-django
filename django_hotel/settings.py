4a7acb30a7918668cb6139f0b55a4aaf274ffbbc
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-09-08 21:22:32 -0300

    Creacion de templates

--- django_hotel/settings.py
+++ django_hotel/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+
 ]
 
 ROOT_URLCONF = 'django_hotel.urls'
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
