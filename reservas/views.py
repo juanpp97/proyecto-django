fb45b0b2991690e4c4355d044c6eb3be23c6d6f6
LautaroMRA02 <lautaro.mra.2002@gmail.com>
2023-09-12 01:11:28 -0300

    [add]: add view facilities with a swiper component

--- reservas/views.py
+++ reservas/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "inicio.html")
+
+def facilities(request):
+    context = {
+        "imagenes": [
+            {"img":"img/facilities/instalaciones_01.webp","titulo":"example 1"},
+            {"img":"img/facilities/instalaciones_02.webp","titulo":"example 2"},
+            {"img":"img/facilities/instalaciones_01.webp","titulo":"example 3"},
+            {"img":"img/facilities/instalaciones_02.webp","titulo":"example 4"}
+            ]
+    }
+
+    # return HttpResponse(context)
+    return render(request, "./facilities/facilities.html", context)
