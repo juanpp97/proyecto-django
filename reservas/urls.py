7840598090e4b8f746e85b06c9aa1fef7916029f
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-09-20 18:18:13 -0300

    Actualizacion de gitignore

--- reservas/urls.py
+++ reservas/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('habitaciones/', views.rooms, name="rooms"),
     path('reserva/<int:id_hab>', views.reservation, name='reservation'),
-
     path('instalaciones/', views.facilities, name='facilities'),
     path('contacto/', views.contact, name='contact'),
 ]
