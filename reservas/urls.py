2da2223fefa0e321a50d925727bc73bfa59d9fd1
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-09-20 16:13:46 -0300

    Formulario de reserva

--- reservas/urls.py
+++ reservas/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('habitaciones/', views.rooms, name="rooms"),
+    path('reserva/<int:id_hab>', views.reservation, name='reservation'),
+
     path('instalaciones/', views.facilities, name='facilities'),
     path('contacto/', views.contact, name='contact'),
 ]
---
+++ static/js/validacion.js
@@ -0,0 +1,33 @@
+const dateIn = document.querySelector('#date_in');
+const dateOut = document.querySelector('#date_out');
+const contador = document.querySelector(".word-counter")
+const comentario = document.querySelector("#comment")
+const maxLength = 1000;
+comentario.maxLength = maxLength;
+contador.innerHTML = maxLength
+dateInMax = dateIn.max;
+dateInMin = dateIn.min;
+
+
+/* Cambio las fechas mínimas y máximas según selección*/
+dateIn.addEventListener('change', (e) => {
+  if(dateIn.value > dateInMin && dateIn.value < dateInMax){
+    dateOut.value = ""
+    let minDateOut = new Date(dateIn.value);
+    let maxDateOut = new Date(dateIn.value);
+    minDateOut.setDate(minDateOut.getDate() + 1);
+    maxDateOut.setMonth(maxDateOut.getMonth() + 2); //Le sumo dos meses como máximo de estadía
+    minDateOut = minDateOut.toISOString().split('T')[0];
+    maxDateOut = maxDateOut.toISOString().split('T')[0];
+    dateOut.min = minDateOut;
+    dateOut.max = maxDateOut;
+    dateOut.removeAttribute('disabled')
+  }else{
+    dateOut.setAttribute('disabled', 'true')
+    dateOut.value = ""
+  }
+});
+comentario.addEventListener("input", () => {
+  /* Si se excede del número máximo de caracteres lo indica en pantalla */
+  contador.innerHTML = maxLength - comentario.value.length;
+});
