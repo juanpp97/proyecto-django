9321da35e2b7c96450dfeeb3fdf87ee6a7dbde0d
LautaroMRA02 <lautaro.mra.2002@gmail.com>
2023-09-30 00:42:31 -0300

    [test] test 2

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 #TEST GIT BOT
+#TEST GIT BOT 2
 urlpatterns = [
     path('', views.index, name="index"),
     path('habitaciones/', views.rooms, name="rooms"),
