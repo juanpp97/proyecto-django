86527baf4ea47c3a62c2bb6c7a9f20f6c71c62ce
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-10-04 21:07:31 -0300

    Actualizacion de requirements.txt

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-#TEST GIT BOT
-#TEST GIT BOT 2
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('habitaciones/', views.rooms, name="rooms"),
