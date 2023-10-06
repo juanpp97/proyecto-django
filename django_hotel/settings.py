aeb0d28997f6b9a2eab796ea6e5ad824a57d5398
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-10-06 15:31:37 -0300

    .

--- django_hotel/settings.py
+++ django_hotel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9q#wo3nftrvfzss+!2si)91e6m_#d)+c3ccxz-nnzt-x=#jp+%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
