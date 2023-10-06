99c592e2433560de7e8a0fd60813f602a6accea5
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-10-05 22:51:47 -0300

    Migraciones

--- django_hotel/settings.py
+++ django_hotel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9q#wo3nftrvfzss+!2si)91e6m_#d)+c3ccxz-nnzt-x=#jp+%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +77,14 @@ WSGI_APPLICATION = 'django_hotel.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+    }
 }
 
 
--- reservas/forms.py
+++ reservas/forms.py
@@ -14,14 +14,21 @@ choices = (('frente', 'Frente'), ('playa', 'Playa'))
 
 
 class ReservationForm(forms.Form):
+    id_hab = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'type': 'hidden'}), error_messages=default_errors)
 
-    num_people = forms.IntegerField(label="Número de personas", widget=forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'value': '1',}), error_messages=default_errors)
+    num_people = forms.IntegerField(label="Número de personas", widget=forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'value': 1}), error_messages=default_errors)
 
     room_view = forms.CharField(label="Vista", widget=forms.Select(attrs={'class': 'form-control'}, choices=choices), error_messages=default_errors)
 
-    date_in = forms.DateField(widget=forms.DateInput(attrs={"class": "form-control", "type": "date", "max": "2023-12-31"}), label="Fecha: ", error_messages=default_errors)
+    date_in = forms.DateField(widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}), label="Fecha de Ingreso", error_messages=default_errors)
 
+    date_out = forms.DateField(widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}), label="Fecha de Salida", error_messages=default_errors)
 
+    def clean_id_hab(self):
+        data = self.cleaned_data["id_hab"]
+        if data != self.fields["id"].widget.attrs["value"]:
+            raise ValidationError("Tipo de habitación no válido")
+        
     def clean_num_people(self):
         num_people = self.cleaned_data["num_people"]
         min = self.fields["num_people"].widget.attrs["min"]
@@ -31,13 +38,24 @@ class ReservationForm(forms.Form):
 
 
     def __init__(self, *args, **kwargs):
+        id_hab = kwargs.pop("id_hab", None)
         capacity = kwargs.pop("capacity", None)
         room_view = kwargs.pop("room_view", None)
+        min_date_in = kwargs.pop("min_date_in", None)
+        max_date_in = kwargs.pop("max_date_in", None)
+        max_date_out = kwargs.pop("max_date_out", None)
+        
         super(ReservationForm, self).__init__(*args, **kwargs)
+        if id_hab:
+            self.fields['id_hab'].widget.attrs['value'] = id_hab
         if capacity:
             self.fields['room_view'].widget.attrs['max'] = capacity
         if room_view:
-            self.fields['room_view'].widget.choices = capacity
+            self.fields['room_view'] = forms.CharField(label="Vista", widget=forms.TextInput(attrs={'class': 'form-control-plaintext', 'readonly': 'True', 'value': room_view}), error_messages=default_errors)
+        if max_date_in and min_date_in:
+            self.fields['date_in'].widget.attrs['max'] = max_date_in
+            self.fields['date_in'].widget.attrs['min'] = min_date_in
+            
 
 
 
