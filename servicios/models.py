6702bdf8997ff866291837f274a71a95db75ddc9
Gabriel <gabrielignacioetchegoyen@hotmail.com>
2023-10-20 20:38:14 -0300

    json productos

--- servicios/models.py
+++ servicios/models.py
@@ -15,7 +15,7 @@ class Eventos(models.Model):
 
 class Producto(models.Model):
 
-    opciones_categoria = [('Platos de Autor','Platos de Autor'),('Opciones rapidas','Opciones rapidas'),('Cocteles y tragos','Cocteles y tragos'),('Whiskeys','Whiskeys'),('Cofee','Cofee'),('Non Cofee','Non Cofee'), ('Smoothie','Smoothie'),('Bakery and pastry','Bakery and pastry')]
+    opciones_categoria = [('Comidas','Comidas'),('Cafeteria','Cafeteria' ),('Bebidas','Bebidas'),('Platos de Autor','Platos de Autor'),('Opciones rapidas','Opciones rapidas'),('Cocteles y tragos','Cocteles y tragos'),('Whiskeys','Whiskeys'),('Cofee','Cofee'),('Non Cofee','Non Cofee'), ('Smoothie','Smoothie'),('Bakery and pastry','Bakery and pastry')]
 
     nombre = models.CharField(verbose_name="nombre", max_length=100 , null= False, blank=False)
     categoria = models.CharField(verbose_name="categoria", max_length=100 , null= False, blank=False , choices= opciones_categoria)
