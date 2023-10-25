ed84d53d3f0aca3025a3bd926c7ea85ac32a59b9
Juan P. Peñaloza <juanpenaloza97@gmail.com>
2023-10-25 12:02:05 -0300

    Correccion de error al editar habitación

--- administracion/urls.py
+++ administracion/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('habitaciones/crear', views.RoomCreateView.as_view(), name = 'crear_hab'),
     path('habitaciones/editar/<int:pk>', views.RoomUpdateView.as_view(), name = 'editar_hab'),
     path('habitaciones/eliminar/<int:pk>', views.RoomDeleteView.as_view(), name = 'eliminar_hab'),
+    path('vista/', views.RoomViewListView.as_view(), name = 'listar_vista'),
     path('vista/crear', views.RoomViewCreateView.as_view(), name = 'crear_vista'),
+    path('vista/editar/<int:pk>', views.RoomViewCreateView.as_view(), name = 'editar_vista'),
+    path('vista/eliminar/<int:pk>', views.RoomViewCreateView.as_view(), name = 'eliminar_vista'),
     
     
 ] 
--- administracion/views.py
+++ administracion/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -13,6 +15,11 @@ class RoomListView(ListView):
     context_object_name = 'rooms_list'
     template_name = 'administracion/listar_hab.html'
     ordering = ['capacity']
+    def get(self, request, *args, **kwargs):
+        if 'ordering' in request.GET:
+            print(request.GET["ordering"])
+            self.ordering = [request.GET["ordering"]]
+        return super().get(request, *args, **kwargs)
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["date"] = datetime.now()
@@ -42,7 +49,7 @@ class RoomCreateView(CreateView):
         return super().form_invalid(form)
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["titulo"] = "Crear Nueva Habitación"
+        context["titulo"] = "Nuevo Tipo de Habitación"
         context["boton"] = "Crear"
         
         context["date"] = datetime.now()
@@ -80,7 +87,7 @@ class RoomUpdateView(UpdateView):
                     ruta.append(nuevo_nombre)
                     path_ant = imagen.img.path
                     imagen.img.name =  "/".join(ruta)
-                    rename(path_ant, imagen.ruta.path)
+                    rename(path_ant, imagen.img.path)
                     imagen.save()
 
             if self.request.FILES.getlist('imgs'):
@@ -98,17 +105,17 @@ class RoomUpdateView(UpdateView):
             room.save()
             messages.success(self.request, "Los datos se han modificado correctamente")
         except BaseException as e:
-            messages.error(self.request, "Ha ocurrido un error")
+            print(e)
             return self.form_invalid(form)
         return super().form_valid(form)
     
     def form_invalid(self, form):
         messages.error(self.request, "Ha ocurrido un error al modificar los datos")
         return super().form_invalid(form)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["titulo"] = "Editar Habitación"
+        context["titulo"] = "Editar Tipo de Habitación"
         context["date"] = datetime.now()
         context["boton"] = "Editar"
         return context
@@ -119,6 +126,7 @@ class RoomDeleteView(DeleteView):
     template_name = 'administracion/eliminar.html'
     success_url = reverse_lazy('listar_hab')
     context_object_name = 'room'
+
     def form_valid(self, form):
         if RoomImg.objects.filter(room = self.object).exists():
             for image in RoomImg.objects.filter(room = self.object):
@@ -126,6 +134,19 @@ class RoomDeleteView(DeleteView):
                     remove(image.img.path)
         return super().form_valid(form)
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["date"] = datetime.now()
+        context["titulo"] = "Eliminar Habitación"
+        context["url"] = reverse_lazy("listar_hab")
+        context["aviso"] = f"¿Está seguro que desea eliminar permanentemente la {self.object.name}?"
+        return context
+    
+
+class RoomViewListView(ListView):
+    model = RoomView
+    template_name = 'administracion/listar_vistas.html'
+    context_object_name = 'views_list'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["date"] = datetime.now()
