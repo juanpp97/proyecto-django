b3cb90689bc4acdb16bdcd3a7045b86019b4ccd9
Gabriel <gabrielignacioetchegoyen@hotmail.com>
2023-11-07 00:07:11 -0300

    actualizacion url de servicio y estilos

--- servicios/urls.py
+++ servicios/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('inicio/',views.inicio, name= 'inicio'),
+    path('habitacion/productos/', views.habitacion, name="productos"),
     path('habitacion/productos/<str:tipo>', views.habitacion, name="serv-producto"),
     # path('habitacion/carrito', views.carrito, name="serv-carrito"),
     path('actividades/', views.actividades, name='actividades'),
+    
     
     
 ]
