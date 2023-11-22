ae179d56fcd5a683e87066a60d01472fee2aa293
Gabriel <gabrielignacioetchegoyen@hotmail.com>
2023-11-22 00:14:16 -0300

    eventos

--- servicios/models.py
+++ servicios/models.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 # Create your models here.
 class Eventos(models.Model):
     titulo = models.CharField(verbose_name='Titulo', max_length=44, null=False, blank=False)
-    informacion = models.CharField(verbose_name='Informacion',max_length=144, null=False, blank=True)
-    imagen = models.URLField(verbose_name='Imagen', default='https://placehold.co/600x400/webp')
-    lugar = models.CharField(verbose_name='Lugar',max_length=44, null=True, blank=True, default='Preguntar')
+    informacion = models.CharField(verbose_name='Informacion',max_length=244, null=False, blank=True)
+    imagen = models.ImageField(verbose_name='Imagen', upload_to='eventos/')
+    lugar = models.CharField(verbose_name='Lugar',max_length=144, null=True, blank=True, default='Preguntar')
     fecha = models.DateField(default=timezone.now)
     # contenido = models.JSONField()
 
