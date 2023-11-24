220432f936787b795aafdb4f40d92393c955683e
LautaroMRA02 <lautaro.mra.2002@gmail.com>
2023-11-24 16:29:49 -0300

    [add]: add Model admin en administracion

--- administracion/admin.py
+++ administracion/admin.py
@@ -3,6 +3,26 @@ from .models import RoomImg, RoomType, RoomView, Price
 # Register your models here.
 
 admin.site.register(RoomImg)
-admin.site.register(RoomType)
+# admin.site.register(RoomType)
 admin.site.register(RoomView)
 admin.site.register(Price)
+
+
+class Imagen_linkeada(admin.TabularInline):
+    model = RoomImg
+    extra = 1
+
+@admin.register(RoomType)
+class RomTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_view','capacity' , 'num_beds')
+    inlines = [Imagen_linkeada]
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'capacity', 'num_beds')
+        }), 
+        (None, {
+            'fields': ('view',),
+        }),
+        )
+    
+    filter_horizontal = ('view',)
